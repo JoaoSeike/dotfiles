4b7013baf973fd4258d1ed1122bdce177aeaf44e
Joao Seike <jgseike@Joaos-MacBook-Pro.local>
2026-04-27 19:41:22 -0300

    oil added

--- nvim/lua/jseike/plugins/telescope.lua
+++ nvim/lua/jseike/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 
     'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
     local telescope = require("telescope").setup({})
